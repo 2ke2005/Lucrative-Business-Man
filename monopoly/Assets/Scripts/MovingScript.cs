91353f91f4148eadf712b29ba9f3a3ac0de1c8e5
BiglerGaming <81150978+BiglerGaming@users.noreply.github.com>
2022-09-03 22:17:16 +0200

    Player 2 added

--- monopoly/Assets/Scripts/MovingScript.cs
+++ monopoly/Assets/Scripts/MovingScript.cs
@@ -6,30 +6,73 @@ using UnityEngine;
 
 public class MovingScript : MonoBehaviour
 {
+    public void rollDice()
+    {
+        if (activePlayerIs1 == true) {
+            activePlayerLoc = player1Loc;
+        }
+        else
+        {
+            activePlayerLoc = player2Loc; 
+        }
 
+       
+        // get random amount to move
+        int gayAmount = Random.Range(1, 6);
+        // add dice roll to location of player
+        activePlayerLoc = activePlayerLoc + gayAmount;
+        // make sure it loops
+        if (locas.Length - 1 < activePlayerLoc)
+        {
+            activePlayerLoc = activePlayerLoc - locas.Length;
+        }
+        // update location 
+        if (activePlayerIs1)
+        {
+            player1Loc = activePlayerLoc;
+        }
+        else
+        {
+            player2Loc = activePlayerLoc;
+        }
+
+
+
+        // Move the player object
+        
+        
+        player1.position = locas[player1Loc].position;
+        player2.position = locas[player2Loc].position;
+        
+
+        // change turn 
+        activePlayerIs1 = !activePlayerIs1;
+
+    }
+    public int activePlayerLoc = 0;
     [SerializeField] private Transform[] locas;
-    [SerializeField] private Transform player;
-    [SerializeField] private int playerPos; 
+    [SerializeField] private Transform player1;
+    [SerializeField] private Transform player2;
+    [SerializeField] private int player1Loc;
+    [SerializeField] private int player2Loc;
+    [SerializeField] private bool activePlayerIs1;
     // Start is called before the first frame update
     void Start()
     {
-        player.position = locas[0].position;
-        playerPos = 0;
+        // active player is  one
+        activePlayerIs1 = true;
+
+        player1.position = locas[0].position;
+        player1Loc = 0;   
+        player2Loc = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("m"))
+        /*if (Input.GetKeyDown("m"))
         {
-            int gayAmount = Random.Range(1, 6);
-            playerPos = playerPos + gayAmount;
-            if (locas.Length -1 < playerPos)
-            {
-                playerPos = playerPos - locas.Length;
-            }
-            player.position = locas[playerPos].position;
-            
-        }
+            rollDice();
+        }*/
     }
 }
